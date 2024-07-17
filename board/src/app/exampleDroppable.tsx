ff869a1f11ac6c63d463523610e55d8d4b8e3952
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-07-18 00:15:31 +0200

    refactor(board): externalize sensors handling to wrapper component, removed obsolete files

--- board/src/app/exampleDroppable.tsx
+++
@@ -1,9 +0,0 @@
-import {useDroppable} from "@dnd-kit/core"
-
-export const MyDroppableComponent = () => {
-  const {setNodeRef} = useDroppable({
-    id: 'droppable-1'
-  })
-
-  return <div ref={setNodeRef}> Drop on me! </div>
-}
--- board/src/app/sensors.tsx
+++ board/src/app/sensors.tsx
@@ -1,10 +1,16 @@
-import {DndContext, MouseSensor, TouchSensor, useSensor, useSensors} from '@dnd-kit/core';
+import {
+  DndContext,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import { ComponentProps } from 'react';
 
-export const DragDropContainer = () => {
-
+export const DragDropContainer = (props: ComponentProps<typeof DndContext>) => {
   const mouseSensor = useSensor(MouseSensor);
   const touchSensor = useSensor(TouchSensor);
-  const sensors = useSensors(mouseSensor, touchSensor)
+  const sensors = useSensors(mouseSensor, touchSensor);
 
-  return (<DndContext sensors={sensors}>.....</DndContext>)
-}
+  return <DndContext {...props} {...{ sensors }} />;
+};
